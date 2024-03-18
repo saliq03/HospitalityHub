17903ace5f53dd6930c25ff2cfa59fa1c8213b4f
saliq03 <javidsaliq@gmail.com>
2024-03-18 23:59:19 +0530

    second commit

--- app/src/main/java/com/csit/hospotalityhub/HandilingActivity.java
+++ app/src/main/java/com/csit/hospotalityhub/HandilingActivity.java
@@ -2,13 +2,26 @@ package com.csit.hospotalityhub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class HandilingActivity extends AppCompatActivity {
 
+    LinearLayout AddRoom;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_handiling);
+       AddRoom=findViewById(R.id.add_room);
+       AddRoom.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent addroomIntent=new Intent(HandilingActivity.this, RoomAddActivity.class);
+               startActivity(addroomIntent);
+
+           }
+       });
     }
 }
---
+++ app/src/main/java/com/csit/hospotalityhub/RoomAddActivity.java
@@ -0,0 +1,21 @@
+package com.csit.hospotalityhub;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class RoomAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_room_add);
+
+
+    }
+}
